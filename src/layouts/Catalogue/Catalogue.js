1ecb8ce801b3154b78c2219423c1d6efefcdb5bd
irenewn <irenewemonan@gmail.com>
2021-10-13 00:38:32 +0700

    create cart function

--- src/layouts/Catalogue/Catalogue.js
+++ src/layouts/Catalogue/Catalogue.js
@@ -1,7 +1,7 @@
 import React, {Component } from "react";
 import Product from "../../components/Product";
 import { ProductApi } from "../../api";
-import { Modal, Button, InputNumber, Input } from "antd";
+import { Modal, Button, InputNumber, } from "antd";
 import 'antd/dist/antd.css';
 
 const loading = (
@@ -19,7 +19,8 @@ export default class Catalogue extends Component {
     onLoading: false,
     showModal: false,
     showModalCart: false,
-    cart: []
+    cart: [],
+    qty: 1,
   };
 
   handleMenuOpen(e) {
@@ -49,13 +50,118 @@ export default class Catalogue extends Component {
   handleOk = () => {
     this.setState({ loading: true });
     setTimeout(() => {
-      this.setState({ loading: false, showModal: false, selectedProduct: {} });
+      this.setState({ loading: false, showModal: false, selectedProduct: {} , qty: 1});
     }, 3000);
   };
 
   handleCancel = () => {
-    this.setState({ showModal: false, selectedProduct: {} });
+    this.setState({ showModal: false, selectedProduct: {}, qty: 1 });
   };
+
+  handleOkCart = () => {
+    this.setState({ showModalCart: false });
+  };
+
+  handleCancelCart = () => {
+    this.setState({ showModalCart: false });
+  };
+
+  addtoCartHandler = () => {
+    var carts = this.state.cart
+    var qty = this.state.qty
+    var position = carts
+    .map(function (e) {
+      return e.id;
+    })
+    .indexOf(this.state.selectedProduct.id);
+    if (position<0){
+      //new insert
+      var objInserted = {
+        ...this.state.selectedProduct,
+        qty,
+      };
+      carts.push(objInserted)
+    }else{
+      var obj = carts[position];
+      var objInserted = {...obj, qty: obj.qty+this.state.qty};
+      carts[position] = objInserted;
+    }
+    this.setState({
+      showModal: false,
+      qty: 1,
+      selectedProduct: {},
+    },()=>{
+      this.recalculateGrandTotal();
+      console.log(`cart`, this.state.cart)
+    })
+  }
+
+  handlerEmptyCart(){
+    this.setState({
+      cart: [],
+    })
+  }
+
+  renderModalCart(){
+    return(
+      <Modal
+      centered
+      title="Cart"
+      visible={this.state.showModalCart}
+      onOk={this.handleOkCart}
+      onCancel={this.handleCancelCart}
+      >
+      {(this.state.cart.length == 0)?
+        <div>Your cart is Empty</div>:
+        <div>
+          {this.state.cart.map((product,index)=>{
+            return(
+              <div style={{ display: "flex", flexDirection: "row", margin: "30px" }}>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <img src={product.image}
+                      style={{ height: "150px" }}></img>
+                  <div style={{ margin: "20px", padding: "10px" }}>
+                    <div><b>{product.title}</b></div>
+                    <div style={{ fontSize: "12px" }}>
+                      <b>{product.qty} x ${product.price}</b><br/>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style={{ display:"flex", alignItems:"center", justifyContent:"flex-end"}}
+                >
+                  ${product.qty*product.price}
+                </div>
+              </div>
+            )
+          })
+        }
+        <Button onClick={()=>{this.handlerEmptyCart()}}>EmptyCart</Button>
+        </div>
+        }
+    </Modal>
+    )
+  } 
+
+  recalculateGrandTotal = () => {
+    var grandTotal = 0.0, itemTotal = 0;
+    
+    this.state.cart.map((v) => {
+      grandTotal += v.qty*v.price;
+      itemTotal += v.qty;
+    })
+
+    this.setState({
+      grandTotal,
+      itemTotal,
+    })
+  }
+
+  onChangeQty(value){
+    this.setState({
+      qty: value
+    })
+  }
 
   renderModalProduct(){
     return(
@@ -87,7 +193,7 @@ export default class Catalogue extends Component {
               <b>${this.state.selectedProduct.price}</b><br/>
             </div>
             <div>
-              <InputNumber min={1} defaultValue={1} />
+              <InputNumber min={1} defaultValue={1} onChange={(value) => {console.log("Number Value :",value); this.onChangeQty(value)}}/>
             </div>
           </div>
         </div>
@@ -113,8 +219,8 @@ export default class Catalogue extends Component {
               </li>
             </ul>
           </div>
-          <div className="navbar-right">
-            Cart
+          <div className="navbar-right" onClick={()=>this.setState({showModalCart: true})}>
+            Carts - {this.state.cart.length == 0 ? `0 items` : `${this.state.itemTotal} items`} ($ {this.state.cart.length==0 ? '0' : this.state.grandTotal})
           </div>
         </div>
       </section>
@@ -129,6 +235,7 @@ export default class Catalogue extends Component {
         <div className="section-title">Katalog Produk</div>
         <div className="section-content">
         {this.renderModalProduct()}
+        {this.renderModalCart()}
           <div className="products">
             {this.state.products.map((product, index) => {
                   return (
